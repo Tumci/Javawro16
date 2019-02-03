9d76b9a5f02e24139695e797c9644a9dbc925092
rafal.zientara <rzientara@blackberry.com>
2019-02-03 14:56:02 +0100

    search index

--- src/main/java/pl/com/sda/rafal/zientara/apps/lesson1/search/MainSearch.java
+++ src/main/java/pl/com/sda/rafal/zientara/apps/lesson1/search/MainSearch.java
@@ -4,12 +4,23 @@ public class MainSearch {
 
     public static void main(String[] args) {
         int[] input = new int[]{1, 4, 78, 3};
-
         int wantedNumber = 1;
+        int ouputIndex = getIndex(input, wantedNumber);
+        System.out.println("output=" + ouputIndex);
+    }
 
-        int ouputIndex = 0;
-
-
-
+    private static int getIndex( int[] input, int wanted) {
+        for (int i = 0; i < input.length; i++) {
+            System.out.println("i=" + i);
+            int current = input[i];
+            System.out.println("current=" + current);
+            if (wanted == input[i]) {
+                return i;
+            } else {
+                System.out.println("To nie to");
+            }
+            System.out.println();
+        }
+        return -1;
     }
 }
